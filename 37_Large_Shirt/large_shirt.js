78f2bcba9b43215f41b5f87e2b527c3f47c29748
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-08 03:57:30 -0700

    Update large_shirt.js

--- 37_Large_Shirt/large_shirt.js
+++ 37_Large_Shirt/large_shirt.js
@@ -1,25 +1,18 @@
-function make_shirt(message, size = "Large"){
-console.log("The size of the shirt is " + size + " and the message is " + message);
-}
 
-make_shirt("Hello I'm a shirt");
-
-function large_shirt(l_shirt="Large", message="A Big Shirt"){
+function large_shirt(l_shirt="Large", message="I love JS"){
 console.log("The size of the shirt is " + l_shirt + " and the message is " + message);
 
 }
 large_shirt();
 
-function small_shirt(s_shirt="Small", message="A Tiny Shirt"){
+function small_shirt(s_shirt="Small", message="I love JS"){
 console.log("The size of the shirt is " + s_shirt + " and the message is " + message);
 }
 
 small_shirt();
 
+function make_shirt(message, size = "Medium"){
+console.log("The size of the shirt is " + size + " and the message is " + message);
+}
 
-
-
-
-
-
-
+make_shirt("Hello I'm a Medium shirt");
