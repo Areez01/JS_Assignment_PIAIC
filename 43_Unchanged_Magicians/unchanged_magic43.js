1fc3928a7b2befe6c4fafc138ae0c4b647da5daf
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-10 01:35:47 -0700

    Update unchanged_magic43.js

--- 43_Unchanged_Magicians/unchanged_magic43.js
+++ 43_Unchanged_Magicians/unchanged_magic43.js
@@ -1,16 +1,17 @@
 var magicians = ["Mag 1", "Mag 2", "Mag 3"];
+var great_mag = magicians.map(name => `Great ${name}`);
 
 function show_magicians(){
 for(var i = 0; i < magicians.length;i++){
 console.log(magicians[i]);
 }
+}
 
-}
 function make_great(){
-for(var j = 0; j < magicians.length; j++){
-console.log("Great " + magicians[j]);
+for(var h = 0; h < great_mag.length; h++){
+console.log(great_mag[h]);
 }
 }
+
 show_magicians();
 make_great();
-
