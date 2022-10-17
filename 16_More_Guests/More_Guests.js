00addc86647d22f2d1d85d2b1861cf9c133e30a4
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-17 01:12:02 -0700

    Update More_Guests.js

--- 16_More_Guests/More_Guests.js
+++ 16_More_Guests/More_Guests.js
@@ -1,19 +1,20 @@
 const dinnerInvitation = ["Sameer", "Ali", "Abbas"];
-console.log("Mr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
 
+for(let i=0;i<dinnerInvitation.length;i++)
+{
+console.log("Mr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 console.log("\nMr." + dinnerInvitation[1] + ", Sorry to hear that you can't make it to dinner.");
 
 
 dinnerInvitation[1] = "Rizwan"
 
-console.log("\nMr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
+for(i=0;i<dinnerInvitation.length;i++)
+{
+console.log("\nMr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 
-
-console.log("\nHello everyone! We've got more people joining us on dinner.");
+console.log("\nHello everyone! We've got more people joining us on dinner.\n");
 
 dinnerInvitation.unshift("Shahmeer");
 
@@ -21,21 +22,10 @@ dinnerInvitation.splice(1, "Ahsan");
 
 dinnerInvitation.push("Shafiq");
 
-console.log("\nMr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[3] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[4] + ", You're invited for dinner at 8pm");
-
-
+for(i=0;i<dinnerInvitation.length;i++)
+{
+console.log("Mr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 
 
 
