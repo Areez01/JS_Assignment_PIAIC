fc5da946f33c427034d3f83c18d91e8bb473fcbd
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-17 00:59:46 -0700

    Update Guest_List.js

--- 14_Guest_List/Guest_List.js
+++ 14_Guest_List/Guest_List.js
@@ -1,5 +1,4 @@
 const dinnerInvitation = ["Sameer", "Ali", "Abbas"];
-console.log("Mr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
-
+for(let i=0;i<dinnerInvitation.length;i++){
+console.log("Mr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
