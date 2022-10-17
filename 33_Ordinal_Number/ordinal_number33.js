63c395ab41bb7b3e625ecee8954e12bad6f21e39
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-17 04:22:27 -0700

    Update ordinal_number33.js

--- 33_Ordinal_Number/ordinal_number33.js
+++ 33_Ordinal_Number/ordinal_number33.js
@@ -1,56 +1,23 @@
 var ord_num = [1,2,3,4,5,6,7,8,9];
 
-for(var i = 0;i <= 8;i++){
+for(var i=0;i<ord_num.length;i++){
 if(ord_num[i] == 1)
 {
 console.log(""+ord_num[i]+"st");
 }
-if(ord_num[i] == 2){
-console.log(""+ord_num[i]+"nd");
+else if(ord_num[i] == 2)
+{
+  console.log(""+ord_num[i]+"nd");
 }
-if(ord_num[i] == 3){
-console.log(""+ord_num[i]+"rd");
-}
-
-if(ord_num[i] == 4){
-console.log(""+ord_num[i]+"th");
+else if(ord_num[i] == 3)
+{
+  console.log(""+ord_num[i]+"rd");
 }
 
-if(ord_num[i] == 5){
-console.log(""+ord_num[i]+"th");
-}
-if(ord_num[i] == 6){
-console.log(""+ord_num[i]+"th");
-}
-if(ord_num[i] == 7){
-console.log(""+ord_num[i]+"th");
-}
-if(ord_num[i] == 8){
-console.log(""+ord_num[i]+"th");
-}
-if(ord_num[i] == 9){
-console.log(""+ord_num[i]+"th");
-}
-
+else if(ord_num[i] > 3)
+{
+  console.log(""+ord_num[i]+"th");
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
