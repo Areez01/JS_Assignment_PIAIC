7cfde6e5a61090d63b5bf65a896efe74eedeb4b0
Areez01 <41503917+Areez01@users.noreply.github.com>
2022-10-17 01:20:12 -0700

    Update Dinner_Guests.js

--- 19_Dinner_Guests/Dinner_Guests.js
+++ 19_Dinner_Guests/Dinner_Guests.js
@@ -1,17 +1,19 @@
 const dinnerInvitation = ["Sameer", "Ali", "Abbas"];
-console.log("Mr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
+
+for(let i=0;i<dinnerInvitation.length;i++)
+{
+console.log("Mr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 
 console.log("\nMr." + dinnerInvitation[1] + ", Sorry to hear that you can't make it to dinner.");
 
 
 dinnerInvitation[1] = "Rizwan"
 
-console.log("\nMr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
-
+for(i=0;i<dinnerInvitation.length;i++)
+{
+console.log("\nMr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 
 console.log("\nHello everyone! We've got more people joining us on dinner.");
 
@@ -21,20 +23,10 @@ dinnerInvitation.splice(1, "Ahsan");
 
 dinnerInvitation.push("Shafiq");
 
-console.log("\nMr." + dinnerInvitation[0] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[1] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[2] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[3] + ", You're invited for dinner at 8pm");
-
-
-console.log("Mr." + dinnerInvitation[4] + ", You're invited for dinner at 8pm");
-
+for(i=0;i<dinnerInvitation.length;i++)
+{
+console.log("\nMr." + dinnerInvitation[i] + ", You're invited for dinner at 8pm");
+}
 
 console.log("\nI'm really sorry to say that but we can only invite two people for the dinner.");
 
